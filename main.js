4914d92fa6870f78d0cc2a35349f3b539562d927
Unknown <shani.rubovitch@gmail.com>
2019-04-03 21:22:09 +0200

    first try

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+narcissistic(153)
+
 function narcissistic(value) {
+    console.log(value)
     // Code me to return true or false
+    var digitSum = value.toString().split('').map(x => x ** value.length)
+        .reduce(function (accumulator, currentValue) {
+            return accumulator + currentValue
+        })
+    if (Number(digitSum) == value) {
+        console.log("true")
+        return true
+    }
+    else {
+        console.log("false")
+        return false
+    }
 }
