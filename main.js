869e864e80e13a6bf98c0fa45221e0f216578001
Unknown <shani.rubovitch@gmail.com>
2019-04-13 23:16:30 +0200

    working

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 narcissistic(153)
+narcissistic(154)
+narcissistic(7)
+
+
 
 function narcissistic(value) {
     console.log(value)
     // Code me to return true or false
-    var digitSum = value.toString().split('').map(x => x ** value.length)
+    var digitSum = value.toString().split('').map(x => x ** value.toString().length)
         .reduce(function (accumulator, currentValue) {
             return accumulator + currentValue
         })
